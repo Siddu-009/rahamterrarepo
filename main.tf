cd2a49893ab66b54cb82e47a212b89d9944c3bc7
Siddu-009 <siddardha9988@gmail.com>
2026-02-22 11:33:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,6 @@ provider "aws" {
 region = "ap-south-1"
 }
 
-resource "aws_instance" "three" {
-  count         = 1
-  ami           = "ami-051a31ab2f4d498f5"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "test-server"
-  }
-}
+resource "aws_s3_bucket" "three" {
+  bucket = "s3bktfromjenkins"
+  versioning = "enabled"
