34ec7ef30c4e277e2b6d32e3de0e803776510ae1
Siddu-009 <siddardha9988@gmail.com>
2026-02-22 11:36:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
 region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "three" {
+resource "aws_s3_bucket" "two" {
   bucket = "s3bktfromjenkins"
-  versioning = "enabled"
+  versioning = "yes"
+}
