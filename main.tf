cf7009bb248a253674ff6b843cddd12d6ad3e956
Siddu-009 <siddardha9988@gmail.com>
2026-02-22 11:49:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,12 @@ provider "aws" {
 region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "one" {
-  ami = "ami-051a31ab2f4d498f5"
+resource "aws_instance" "three" {
+  count         = 1
+  ami           = "ami-051a31ab2f4d498f5"
   instance_type = "t3.micro"
-  tags{
+
+  tags = {
     Name = "test-server"
   }
 }
